4b76b018d4b6d3cf6827752fb0d6bda7bb5f6531
Soku07 <i2k20103135@ms.pict.edu>
2026-04-11 17:13:05 +0530

    Updated RatelimiterApplication.java

--- src/main/java/com/ratelimiter/RatelimiterApplication.java
+++ src/main/java/com/ratelimiter/RatelimiterApplication.java
@@ -3,9 +3,11 @@ package com.ratelimiter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 @ComponentScan(
         excludeFilters = @ComponentScan.Filter(
                 type = FilterType.REGEX,
