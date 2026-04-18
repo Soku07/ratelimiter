4d2da619538bce0417699bf5306eb1750b76cf99
Soku07 <i2k20103135@ms.pict.edu>
2026-04-18 19:44:50 +0530

    Added Enums to validate input of Policies

---
+++ src/main/java/com/ratelimiter/model/RateLimitSpecs.java
@@ -0,0 +1,14 @@
+package com.ratelimiter.model;
+
+public final class RateLimitSpecs {
+    private RateLimitSpecs() {}
+
+    public enum Identity{
+        IP_ADDRESS,
+        AUTH_TOKEN
+    }
+
+    public enum Algorithm{
+        TOKEN_BUCKET
+    }
+}
