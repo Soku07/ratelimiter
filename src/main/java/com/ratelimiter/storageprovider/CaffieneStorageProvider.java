5044d184679569caea7f7c9926283e02eaec2073
soham-kulkarni <sohamkulkarni0706@gmail.com>
2026-04-28 20:52:25 +0530

    Updated Caffiene Storage Provider

--- src/main/java/com/ratelimiter/storageprovider/CaffieneStorageProvider.java
+++ src/main/java/com/ratelimiter/storageprovider/CaffieneStorageProvider.java
@@ -1,13 +1,18 @@
 package com.ratelimiter.storageprovider;
 
 import com.github.benmanes.caffeine.cache.Cache;
+import com.ratelimiter.algorithm.RateLimitDecision;
 import com.ratelimiter.exceptions.StorageException;
+import com.ratelimiter.model.RateLimitContext;
+import com.ratelimiter.model.RateLimitSpecs;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 import java.time.Duration;
 import java.util.function.BiFunction;
 
 @Component
+//@Primary
 public class CaffieneStorageProvider implements StorageProvider {
     private final Cache<String, Object> cache;
     public CaffieneStorageProvider(Cache<String, Object> cache){
@@ -15,7 +20,7 @@ public class CaffieneStorageProvider implements StorageProvider {
     }
 
     @Override
-    public <T> T atomicCompute(String key, BiFunction<String, T, T> algorithmLogic, Duration timeToLive) {
+    public <T extends RateLimitDecision> T atomicCompute(String key, RateLimitSpecs.Algorithm algorithm, RateLimitContext context, BiFunction<String, T, T> algorithmLogic, Duration timeToLive) {
         @SuppressWarnings("unchecked")
         T result = (T) cache.asMap().compute(
                 key,(k, oldValue) -> {
