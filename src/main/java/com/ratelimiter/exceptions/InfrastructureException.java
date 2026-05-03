7381dc95974fcc46e8c188e16ca9f51f53295894
soham-kulkarni <sohamkulkarni0706@gmail.com>
2026-05-03 10:14:40 +0530

    Changed Exception architecture

--- src/main/java/com/ratelimiter/exceptions/InfrastructureException.java
+++
@@ -1,7 +0,0 @@
-package com.ratelimiter.exceptions;
-
-public class InfrastructureException extends RuntimeException{
-    public InfrastructureException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
