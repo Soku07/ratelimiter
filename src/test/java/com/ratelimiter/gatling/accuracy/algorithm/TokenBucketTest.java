b1eb5026b5f7d363f77a204fb36833e0ba0acf5f
soham-kulkarni <sohamkulkarni0706@gmail.com>
2026-05-23 09:35:43 +0530

    Added a test to verify token refilling

--- src/test/java/com/ratelimiter/gatling/accuracy/algorithm/TokenBucketTest.java
+++ src/test/java/com/ratelimiter/gatling/accuracy/algorithm/TokenBucketTest.java
@@ -4,39 +4,58 @@ import com.ratelimiter.gatling.accuracy.BaseAccuracyTest;
 import io.gatling.javaapi.core.OpenInjectionStep;
 import io.gatling.javaapi.core.ScenarioBuilder;
 
+import java.time.Duration;
 import java.util.List;
 
 public class TokenBucketTest extends BaseAccuracyTest {
     public final double burstFactor;
     public final int expectedMaxCapacity;
+    public final int totalRequestsToFire;
     public TokenBucketTest(){
         this.burstFactor = Double.parseDouble(System.getProperty("BURST_FACTOR", "0.0"));
         List<EndpointRule> rules = loadEnpointAndItsLimitFromCSV();
         EndpointRule target = rules.getFirst();
         this.expectedMaxCapacity = (int) ((1.0 + burstFactor) * target.limit());
-        int totalRequestsToFire = (int) Math.ceil(expectedMaxCapacity * 0.1) + expectedMaxCapacity;
+        this.totalRequestsToFire = (int) Math.ceil(expectedMaxCapacity * 0.1) + expectedMaxCapacity;
 
+        double timePerTokenSeconds = (double) target.window().toSeconds() / target.limit();
+        //we will wait for some delta time so that it is gurateed that atleast 1 token is generated
+        double waitTimeSeconds = timePerTokenSeconds * 1.3;
         ScenarioBuilder scenario = createAccuracyScenario("Token Bucket Accuracy Test", target.method(), target.endpoint(),singleIdentityFeeder);
+        ScenarioBuilder refillTokenScenario = createAccuracyScenario("Refil",target.method(),target.endpoint(),singleIdentityFeeder);
 
-        setUp(scenario.injectOpen(OpenInjectionStep.atOnceUsers(totalRequestsToFire))).protocols(httpProtocol);
+
+
+        setUp(scenario.injectOpen(OpenInjectionStep.atOnceUsers(totalRequestsToFire)),
+                refillTokenScenario.injectOpen(
+                        OpenInjectionStep.nothingFor(Duration.ofMillis((long) waitTimeSeconds * 1000)),
+                        OpenInjectionStep.atOnceUsers(1))
+
+
+        ).protocols(httpProtocol);
+
 
 
     }
 
     @Override
     protected void validateAssertions(int allowed, int throttled) {
-        if (allowed != expectedMaxCapacity) {
+        int minAllowed = expectedMaxCapacity;
+        int maxAllowed = expectedMaxCapacity + 1;
+        int totalVolumeSent = totalRequestsToFire + 1;
+        if (allowed < minAllowed || allowed > maxAllowed || (allowed + throttled) != totalVolumeSent) {
             throw new AssertionError(String.format(
                     "CRITICAL: Token Bucket Math Violation!\n" +
-                            "Expected exactly %d allowed requests to pass through boundary check.\n" +
-                            "Instead, the rate limiter allowed %d requests. Check for rounding errors.",
-                    expectedMaxCapacity, allowed
+                            "Expected total allowed requests to be within the jitter-safe range [%d - %d].\n" +
+                            "Actual System Metrics -> Allowed: %d, Throttled: %d (Total Sent: %d)",
+                    minAllowed, maxAllowed, allowed, throttled, (allowed + throttled)
             ));
         }
+
         System.out.println(String.format(
-                ">> SUCCESS: Micro-accuracy validated perfectly!\n" +
-                        "   Allowed exact capacity: %d | Cleanly blocked extra latency padding: %d",
-                allowed, throttled
+                ">> SUCCESS: Micro-accuracy validated perfectly within clock tolerances!\n" +
+                        "   Actual Allowed: %d (Legal Range: [%d - %d]) | Actual Throttled: %d",
+                allowed, minAllowed, maxAllowed, throttled
         ));
     }
 }
